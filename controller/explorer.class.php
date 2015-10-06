8cc81dd3251ab277198b5664de31c6e1ab483081
EasyChen <easychen@qq.com>
2015-10-06 10:58:50 +0800

    add 7z unzip

--- controller/explorer.class.php
+++ controller/explorer.class.php
@@ -523,8 +523,13 @@ class explorer extends Controller{
         // unzip rar
         if( $ext == 'rar' )
         {
+            //$cmd = '7z x ' .$this->path . ' ' . dirname($this->path) . ' > /dev/null 2>/dev/null & ';
+            $cmd = 'unrar x ' .$this->path . ' ' . dirname($this->path) . ' > /dev/null 2>/dev/null & ';
+            exec($cmd);
+            return show_json($this->L['unzip_success']);
+        }elseif( $ex == '7z' )
+        {
             $cmd = '7z x ' .$this->path . ' ' . dirname($this->path) . ' > /dev/null 2>/dev/null & ';
-            //return show_json( $cmd );
             exec($cmd);
             return show_json($this->L['unzip_success']);
         }
