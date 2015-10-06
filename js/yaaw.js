2ab46b00c77bd4196ba519ce3c5f3114b8f2f6b3
EasyChen <easychen@qq.com>
2015-10-06 11:59:09 +0800

    修改默认rpc协议

--- js/yaaw.js
+++ js/yaaw.js
@@ -808,7 +808,7 @@ var YAAW = (function() {
 
     setting: {
       init: function() {
-        this.jsonrpc_path = $.Storage.get("jsonrpc_path") || location.protocol+"//"+(location.host.split(":")[0]||"localhost")+":6800"+"/jsonrpc";
+        this.jsonrpc_path = $.Storage.get("jsonrpc_path") || location.protocol+"//token:{{token}}@"+(location.host.split(":")[0]||"localhost")+":6800"+"/jsonrpc";
         this.refresh_interval = Number($.Storage.get("refresh_interval") || 10000);
         this.finish_notification = Number($.Storage.get("finish_notification") || 1);
         this.add_task_option = $.Storage.get("add_task_option");
